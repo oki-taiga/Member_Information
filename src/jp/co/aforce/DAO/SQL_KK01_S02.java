534b7bd6b7d8249ea6769e859686cf11c72f8934
oki-taiga <t.oki@a-force.co.jp>
2022-06-10 11:52:54 +0900

    登録画面のみ完成。

--- src/jp/co/aforce/DAO/SQL_KK01_S02.java
+++ src/jp/co/aforce/DAO/SQL_KK01_S02.java
@@ -2,18 +2,26 @@ package jp.co.aforce.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import jp.co.aforce.bean.Member;
 
 public class SQL_KK01_S02 extends DAO{
 	Member m=new Member();
 	public Member search
-	(String last_name,String first_name,int sex,int birth_year,int birth_month,int birth_day,String job,int phone_number,String mail_address)
+	(String member_id)
 			throws Exception{
 		Connection con=getConnection();
 
 		PreparedStatement st=con.prepareStatement("SELECT * FROM MEMBER_INFO WHERE MEMBER_ID=?");
+		st.setString(1,member_id);
+		ResultSet rs=st.executeQuery();
 
+		while(rs.next()) {
+			m.setMember_Id("member_id");
+		}
+		st.close();
+		con.close();
 				return m;
 
 	}
